83ca3afd64c77c97b2ce7dad1d971bf1704ad31b
tmba <ut42.nu@gmail.com>
2025-02-02 18:58:39 +0900

    ABCおよびRFM分析モデルの結果クラスを追加

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -24,3 +24,41 @@ class PurchaseData(BaseModel):
     address: int | None
     age: int | None
     gender: str | None
+
+
+class ResultABC(BaseModel):
+    """
+    Result of ABC Analysis Model
+
+    category: 商品名
+    price: 価格
+    sales: 売上高
+    cumulative_percentage: 累積構成比
+    classed_as: ABCクラス
+    """
+
+    category: str
+    price: float
+    sales: float
+    cumulative_percentage: float
+    classed_as: str
+
+
+class ResultRFM(BaseModel):
+    """
+    Result of RFM Analysis Model
+
+    Recency: 最終購入日からの日数
+    Frequency: 購入回数
+    Monetary: 購入金額
+    R_score: Rスコア
+    F_score: Fスコア
+    M_score: Mスコア
+    """
+
+    recency: int
+    frequency: int
+    monetary: int
+    r_score: int
+    f_score: int
+    m_score: int
